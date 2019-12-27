244a2e1bcdeb7a55a9fb3a6150ee1d27a10aca53
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-12-26 20:06:14 -0500

    Initial commit

---
+++ example/inputsAPI_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:inputsAPI_dart/inputsAPI_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/inputsAPI_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library inputsAPI_dart;
+
+export 'src/inputsAPI_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
